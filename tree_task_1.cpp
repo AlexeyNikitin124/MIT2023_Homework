3e716a55e87081af4b4a38bc28d4d0ba22e5703a
AlexeyNikitin124 <127249788+AlexeyNikitin124@users.noreply.github.com>
2023-05-16 23:33:35 +0400

    Добавлена динамическая структура дерево и функции для работы с ней

---
+++ tree_task_1.cpp
@@ -0,0 +1,31 @@
+﻿// Дано дерево. Удалить четные элементы
+#include <iostream>
+
+using namespace std;
+
+struct tree
+{
+	int inf;
+	tree* left;
+	tree* right;
+	tree* parent;
+};
+
+tree* node(int x) { //создание узла
+	tree* n = new tree;
+	n->inf = x;
+	n->parent = NULL;
+	n->left = NULL;
+	n->right = NULL;
+	return n;
+}
+
+void inorder(tree* tr) { //симметричный обход дерева
+	if (tr) {
+		inorder(tr->left);
+		cout << tr->inf;
+		inorder(tr->right);
+	}
+}
+int main() {
+}
