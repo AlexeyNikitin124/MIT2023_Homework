582fd5f5825cf06e2c51e88eb6a8b4c0bad36b54
AlexeyNikitin124 <127249788+AlexeyNikitin124@users.noreply.github.com>
2023-05-16 23:55:07 +0400

    Добавлена функция создания дерева, объявление дерево и его заполнение

--- tree_task_1.cpp
+++ tree_task_1.cpp
@@ -26,6 +26,25 @@ void inorder(tree* tr) { //симметричный обход дерева
 		cout << tr->inf;
 		inorder(tr->right);
 	}
+
+
+void create(tree*& tr, int n) { //создание дерева
+	int x;
+	if (n > 0) {
+		cin >> x;
+		tr = node(x);
+		int nl = n / 2;
+		int nr = n - nl - 1;
+		create(tr->left, nl);
+		create(tr->right, nr);
+	}
 }
+
 int main() {
+	setlocale(LC_ALL, "RUS");
+	int n, x;
+	tree* tr = NULL;
+	cout << "Введите количество элементов дерева: ";
+	cin >> n;
+	create(tr, n);
 }
