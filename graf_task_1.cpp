cb5a92e9cbddd645b2ffba5bcbaf0e499d7a33dd
AlexeyNikitin124 <127249788+AlexeyNikitin124@users.noreply.github.com>
2023-05-30 00:14:56 +0400

    Добавлена функция создания списка смежности

---
+++ graf_task_1.cpp
@@ -0,0 +1,46 @@
+﻿// Дан неориентированный граф.
+// Вывести на экран все вершины, не смежные с данной.
+#include <iostream>
+#include <map>
+#include <list>
+#include <fstream>
+
+using namespace std;
+
+ifstream in("unoriented.txt");
+
+map<int, list<int>> graph(bool oriented) { //создание списка смежности графа
+    map<int, list<int>> res;
+    int x, y;
+    while (in.peek() != EOF) {
+        in >> x >> y; // вводим ребро в виде x -> y
+        auto it_x = res.find(x);
+        bool fl = true;
+        if (it_x != res.end()) {
+            for (auto it_y = it_x->second.begin();
+                it_y != it_x->second.end(); it_y++)
+                if (it_x->first == y) {
+                    fl = false;
+                    break;
+                }
+        }
+        if (oriented) { // если граф ориентированный
+            if (fl) {
+                res[x].push_back(y);
+                if (res.find(y) == res.end()) {
+                    res[y].push_back(0);
+                    auto it = res.find(y);
+                    it->second.erase(it->second.begin());
+                }
+            }
+        }
+        else if (fl) { // если граф неориентированный
+            res[x].push_back(y);
+            res[y].push_back(x);
+        }
+    }
+    return res;
+}
+
+int main() {
+}
